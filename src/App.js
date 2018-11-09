7c18665f54e094c0a30eec5c687cd5467ba054fc
edmarkou <edvardas.markevicius@gmail.com>
2018-11-09 05:52:56 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ginger from './ginger.svg';
+import squares from './squares.png';
+import gadget from './gadget.png';
 
+const styles = {
+  main: {
+    userSelect: 'none',
+    overflow: 'hidden',
+    display: 'block',
+    position: 'absolute',
+    width: '100%',
+    height: 'auto',
+    transformOrigin: 'left top 0px',
+    background: 'transparent'
+  },
+  headerDiv: {
+    backgroundColor: 'rgb(0, 32, 97)',
+    height: '100px',
+    display: 'flex',
+    alignItems: 'center',
+    position: 'relative'
+  },
+  ginger: {
+    height: '38px',
+    width: '238px',
+    marginLeft: '10%'
+  },
+  helloGingerText: {
+    color: "#FFBF13",
+    marginLeft: '45%',
+    fontWeight: 400,
+    fontFamily: 'Courier, monospace',
+    fontSize: '16px',
+    cursor: 'default',
+  },
+  imageSquareBackground: {
+    height: '695px',
+    width: '105%',
+    backgroundImage: `url(${squares})`,
+    backgroundSize: 'cover',
+    userSelect: 'none',
+    overflow: 'hidden',
+    opacity: '0.2',
+    transform: 'translate(-22px, -70%)'
+  },
+};
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={styles.main}>
+        <div style={styles.headerDiv}>
+          <img src={ginger} style={styles.ginger} alt={""}/>
+          <span style={styles.helloGingerText}>hello@project-ginger.com</span>
+        </div>
+        <div style={{backgroundColor: 'rgba(0, 4, 30, 1.00)'}}>
+          <div style={{overflow: 'hidden'}}>
+            <div>
+                <img style={{width: '65%', transform: 'translate(65%, 10%) rotate(-39deg)'}} src={gadget}/>
+              <div style={styles.imageSquareBackground}/>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
