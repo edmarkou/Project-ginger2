4a980cfce519fafd89b1f7f1fce1dd3ca5772897
edmarkou <edvardas.markevicius@gmail.com>
2018-11-09 06:52:19 +0200

    Second commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ginger from './ginger.svg';
 import squares from './squares.png';
 import gadget from './gadget.png';
+import './ginger.css'
 
 const styles = {
   main: {
@@ -10,8 +11,7 @@ const styles = {
     display: 'block',
     position: 'absolute',
     width: '100%',
-    height: 'auto',
-    transformOrigin: 'left top 0px',
+    height: '100%',
     background: 'transparent'
   },
   headerDiv: {
@@ -35,14 +35,15 @@ const styles = {
     cursor: 'default',
   },
   imageSquareBackground: {
-    height: '695px',
+    height: '105%',
     width: '105%',
     backgroundImage: `url(${squares})`,
     backgroundSize: 'cover',
     userSelect: 'none',
     overflow: 'hidden',
     opacity: '0.2',
-    transform: 'translate(-22px, -70%)'
+    position: 'absolute',
+    transform: 'translate(-1%, -3%)'
   },
 };
 class App extends Component {
@@ -53,12 +54,10 @@ class App extends Component {
           <img src={ginger} style={styles.ginger} alt={""}/>
           <span style={styles.helloGingerText}>hello@project-ginger.com</span>
         </div>
-        <div style={{backgroundColor: 'rgba(0, 4, 30, 1.00)'}}>
-          <div style={{overflow: 'hidden'}}>
-            <div>
-                <img style={{width: '65%', transform: 'translate(65%, 10%) rotate(-39deg)'}} src={gadget}/>
-              <div style={styles.imageSquareBackground}/>
-            </div>
+        <div style={{backgroundColor: 'rgba(0, 4, 30, 1.00)', height: '90%'}}>
+          <div style={{overflow: 'hidden', position: 'relative', height: '100%'}}>
+            <div style={styles.imageSquareBackground}/>
+            <img style={{width: '65%', transform: 'translate(65%, 10%) rotate(-39deg)'}} src={gadget} alt={""}/>
           </div>
         </div>
       </div>
