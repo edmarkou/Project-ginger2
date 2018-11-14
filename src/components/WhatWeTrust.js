978424a38fa543c6194bdee80a5b46852733a07a
edmarkou <edvardas.markevicius@gmail.com>
2018-11-14 21:29:30 +0200

    Finished page

--- src/components/WhatWeTrust.js
+++ src/components/WhatWeTrust.js
@@ -18,15 +18,19 @@ const styles = {
     fontSize: '36px'
   },
   imageIcon: {
-    width: '60px',
     height: '55px',
+    width: '55px',
+    margin: 'auto',
+    backgroundSize: 'contain',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center center',
   },
   iconText: {
     color: 'rgb(250, 206, 51)',
     fontFamily: 'Courier, monospace',
     fontSize: '16px',
     lineHeight: 1.5,
-    marginTop: '15%'
+    marginTop: '10%'
   }
 };
 
@@ -46,14 +50,14 @@ class WhatWeTrust extends Component {
                 <div style={{ width: '90%', paddingTop: '7%', margin: 'auto'}}>
                   <div>
                     <div style={{display: 'inline-grid', width: '50%'}}>
-                      <img style={{...styles.imageIcon, margin: 'auto'}} src={heart} alt={""}/>
+                      <div style={{...styles.imageIcon, backgroundImage: `url(${heart})`}}/>
                       <span style={{ ...styles.iconText, fontSize: '12px', marginTop: '0px', textAlign: 'center'}}>
                             Support what<br/>
                             really loves you
                           </span>
                     </div>
                     <div style={{display: 'inline-grid', width: '50%'}}>
-                      <img style={{...styles.imageIcon, height: '40px', paddingBottom: '7.5px', margin: 'auto'}} src={UI} alt={""}/>
+                      <div style={{...styles.imageIcon, backgroundImage: `url(${UI})`}}/>
                       <span style={{ ...styles.iconText, fontSize: '12px', marginTop: '0px', textAlign: 'center'}}>
                             Awesome user<br/>
                             interface
@@ -62,14 +66,14 @@ class WhatWeTrust extends Component {
                   </div>
                   <div style={{paddingTop: '10%'}}>
                     <div style={{display: 'inline-grid', width: '50%'}}>
-                      <img style={{...styles.imageIcon, margin: 'auto'}} src={rocket} alt={""}/>
+                      <div style={{...styles.imageIcon, backgroundImage: `url(${rocket})`}}/>
                       <span style={{ ...styles.iconText, fontSize: '12px', marginTop: '0px', textAlign: 'center'}}>
                             Fast account<br/>
                             opening
                           </span>
                     </div>
                     <div style={{display: 'inline-grid', width: '50%'}}>
-                      <img style={{...styles.imageIcon, width: '55px', margin: 'auto'}} src={photo} alt={""}/>
+                      <div style={{...styles.imageIcon, backgroundImage: `url(${photo})`}}/>
                       <span style={{ ...styles.iconText, fontSize: '12px', marginTop: '0px', textAlign: 'center'}}>
                             The lowest<br/>
                             fee
@@ -80,14 +84,14 @@ class WhatWeTrust extends Component {
                 <div style={{ width: '90%', paddingTop: '10%', margin: 'auto', userSelect: 'none'}}>
                   <div>
                     <div style={{display: 'inline-grid', width: '50%'}}>
-                      <img style={{...styles.imageIcon, margin: 'auto'}} src={check} alt={""}/>
+                      <div style={{...styles.imageIcon, backgroundImage: `url(${check})`}}/>
                       <span style={{ ...styles.iconText, fontSize: '12px', marginTop: '0px', textAlign: 'center'}}>
                           Easy SEPA<br/>
                           payments
                         </span>
                     </div>
                     <div style={{display: 'inline-grid', width: '50%'}}>
-                      <img style={{...styles.imageIcon, margin: 'auto'}} src={autoRotate} alt={""}/>
+                      <div style={{...styles.imageIcon, backgroundImage: `url(${autoRotate})`}}/>
                       <span style={{ ...styles.iconText, fontSize: '12px', marginTop: '0px', textAlign: 'center'}}>
                           Quick SWIFT<br/>
                           transfers
@@ -96,14 +100,14 @@ class WhatWeTrust extends Component {
                   </div>
                   <div style={{paddingTop: '10%'}}>
                     <div style={{display: 'inline-grid', width: '50%'}}>
-                      <img style={{...styles.imageIcon, height: '50px', paddingBottom: '2.5px', margin: 'auto'}} src={circles} alt={""}/>
+                      <div style={{...styles.imageIcon, backgroundImage: `url(${circles})`}}/>
                       <span style={{ ...styles.iconText, fontSize: '12px', marginTop: '0px', textAlign: 'center'}}>
                           Secure<br/>
                           cryptocurrencies
                         </span>
                     </div>
                     <div style={{display: 'inline-grid', width: '50%'}}>
-                      <img style={{...styles.imageIcon, height: '40px', paddingBottom: '7.5px', margin: 'auto'}} src={ML} alt={""}/>
+                      <div style={{...styles.imageIcon, backgroundImage: `url(${ML})`}}/>
                       <span style={{ ...styles.iconText, fontSize: '12px', marginTop: '0px', textAlign: 'center'}}>
                           Deep machine<br/>
                           learning
@@ -128,68 +132,68 @@ class WhatWeTrust extends Component {
                         <div style={{ width: '90%', paddingTop: '7%', margin: 'auto'}}>
                           <div>
                             <div style={{display: 'inline-grid', width: '160px', marginLeft: '20%'}}>
-                              <img style={styles.imageIcon} src={heart} alt={""}/>
+                              <div style={{...styles.imageIcon, margin: '0', backgroundImage: `url(${heart})`}}/>
                               <span style={styles.iconText}>
-                            Support what<br/>
-                            really loves you
-                          </span>
+                                Support what<br/>
+                                really loves you
+                              </span>
                             </div>
                             <div style={{display: 'inline-grid', marginLeft: '20%', width: '160px'}}>
-                              <img style={{...styles.imageIcon, height: '40px', paddingBottom: '7.5px'}} src={UI} alt={""}/>
+                              <div style={{...styles.imageIcon, margin: '0', backgroundImage: `url(${UI})`}}/>
                               <span style={styles.iconText}>
-                            Awesome user<br/>
-                            interface
-                          </span>
+                                Awesome user<br/>
+                                interface
+                              </span>
                             </div>
                           </div>
                           <div style={{paddingTop: '3%'}}>
                             <div style={{display: 'inline-grid', marginLeft: '20%', width: '160px'}}>
-                              <img style={styles.imageIcon} src={rocket} alt={""}/>
+                              <div style={{...styles.imageIcon, margin: '0', backgroundImage: `url(${rocket})`}}/>
                               <span style={styles.iconText}>
-                            Fast account<br/>
-                            opening
-                          </span>
+                                Fast account<br/>
+                                opening
+                              </span>
                             </div>
                             <div style={{display: 'inline-grid', marginLeft: '20%', width: '160px'}}>
-                              <img style={{...styles.imageIcon, width: '55px'}} src={photo} alt={""}/>
+                              <div style={{...styles.imageIcon, margin: '0', backgroundImage: `url(${photo})`}}/>
                               <span style={styles.iconText}>
-                            The lowest<br/>
-                            fee
-                          </span>
+                                The lowest<br/>
+                                fee
+                              </span>
                             </div>
                           </div>
                         </div>
                         <div style={{ width: '90%', paddingTop: '3%', margin: 'auto', userSelect: 'none'}}>
                           <div>
                             <div style={{display: 'inline-grid', marginLeft: '20%', width: '160px'}}>
-                              <img style={styles.imageIcon} src={check} alt={""}/>
+                              <div style={{...styles.imageIcon, margin: '0', backgroundImage: `url(${check})`}}/>
                               <span style={styles.iconText}>
-                          Easy SEPA<br/>
-                          payments
-                        </span>
+                                Easy SEPA<br/>
+                                payments
+                              </span>
                             </div>
                             <div style={{display: 'inline-grid', marginLeft: '20%', width: '160px'}}>
-                              <img style={styles.imageIcon} src={autoRotate} alt={""}/>
+                              <div style={{...styles.imageIcon, margin: '0', backgroundImage: `url(${autoRotate})`}}/>
                               <span style={styles.iconText}>
-                          Quick SWIFT<br/>
-                          transfers
-                        </span>
+                                Quick SWIFT<br/>
+                                transfers
+                              </span>
                             </div>
                           </div>
                           <div style={{paddingTop: '3%'}}>
                             <div style={{display: 'inline-grid', marginLeft: '20%', width: '160px'}}>
-                              <img style={{...styles.imageIcon, height: '50px', paddingBottom: '2.5px'}} src={circles} alt={""}/>
+                              <div style={{...styles.imageIcon, margin: '0', backgroundImage: `url(${circles})`}}/>
                               <span style={styles.iconText}>
-                          Secure<br/>
-                          cryptocurrencies
-                        </span>
+                                Secure<br/>
+                                cryptocurrencies
+                              </span>
                             </div>
                             <div style={{display: 'inline-grid', marginLeft: '20%', width: '160px'}}>
-                              <img style={{...styles.imageIcon, height: '40px', paddingBottom: '7.5px'}} src={ML} alt={""}/>
+                              <div style={{...styles.imageIcon, margin: '0', backgroundImage: `url(${ML})`}}/>
                               <span style={styles.iconText}>
-                          Deep machine<br/>
-                          learning
-                        </span>
+                                Deep machine<br/>
+                                learning
+                              </span>
                             </div>
                           </div>
                         </div>
@@ -205,28 +209,28 @@ class WhatWeTrust extends Component {
                         </div>
                         <div style={{ width: '72%', paddingTop: '7%', margin: 'auto'}}>
                           <div style={{display: 'inline-grid', width: '17%'}}>
-                            <img style={styles.imageIcon} src={heart} alt={""}/>
+                            <div style={{...styles.imageIcon, margin: '0', backgroundImage: `url(${heart})`}}/>
                             <span style={styles.iconText}>
                               Support what<br/>
                               really loves you
                             </span>
                           </div>
                           <div style={{display: 'inline-grid', marginLeft: '10%', width: '17%'}}>
-                            <img style={{...styles.imageIcon, height: '40px', paddingBottom: '7.5px'}} src={UI} alt={""}/>
+                            <div style={{...styles.imageIcon, margin: '0', backgroundImage: `url(${UI})`}}/>
                             <span style={styles.iconText}>
                               Awesome user<br/>
                               interface
                             </span>
                           </div>
                           <div style={{display: 'inline-grid', marginLeft: '10%', width: '17%'}}>
-                            <img style={styles.imageIcon} src={rocket} alt={""}/>
+                            <div style={{...styles.imageIcon, margin: '0', backgroundImage: `url(${rocket})`}}/>
                             <span style={styles.iconText}>
                               Fast account<br/>
                               opening
                             </span>
                           </div>
                           <div style={{display: 'inline-grid', marginLeft: '10%', width: '17%'}}>
-                            <img style={{...styles.imageIcon, width: '55px'}} src={photo} alt={""}/>
+                            <div style={{...styles.imageIcon, margin: '0', backgroundImage: `url(${photo})`}}/>
                             <span style={styles.iconText}>
                               The lowest<br/>
                               fee
@@ -235,28 +239,27 @@ class WhatWeTrust extends Component {
                         </div>
                         <div style={{ width: '72%', paddingTop: '5%', margin: 'auto', userSelect: 'none'}}>
                           <div style={{display: 'inline-grid', width: '17%'}}>
-                            <img style={styles.imageIcon} src={check} alt={""}/>
-                            <span style={styles.iconText}>
+                            <div style={{...styles.imageIcon, margin: '0', backgroundImage: `url(${check})`}}/>                            <span style={styles.iconText}>
                               Easy SEPA<br/>
                               payments
                             </span>
                           </div>
                           <div style={{display: 'inline-grid', marginLeft: '10%', width: '17%'}}>
-                            <img style={styles.imageIcon} src={autoRotate} alt={""}/>
+                            <div style={{...styles.imageIcon, margin: '0', backgroundImage: `url(${autoRotate})`}}/>
                             <span style={styles.iconText}>
                               Quick SWIFT<br/>
                               transfers
                             </span>
                           </div>
                           <div style={{display: 'inline-grid', marginLeft: '10%', width: '17%'}}>
-                            <img style={{...styles.imageIcon, height: '50px', paddingBottom: '2.5px'}} src={circles} alt={""}/>
+                            <div style={{...styles.imageIcon, margin: '0', backgroundImage: `url(${circles})`}}/>
                             <span style={styles.iconText}>
                               Secure<br/>
                               cryptocurrencies
                             </span>
                           </div>
                           <div style={{display: 'inline-grid', marginLeft: '10%', width: '17%'}}>
-                            <img style={{...styles.imageIcon, height: '40px', paddingBottom: '7.5px'}} src={ML} alt={""}/>
+                            <div style={{...styles.imageIcon, margin: '0', backgroundImage: `url(${ML})`}}/>
                             <span style={styles.iconText}>
                               Deep machine<br/>
                               learning
